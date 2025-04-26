f65a09bc3f9c81a4d71a97607c14023124ae61d9
takuaki <tatennisku@gmail.com>
2025-04-26 10:57:25 +0900

    aaaa 22

--- frontend2/next.config.ts
+++ frontend2/next.config.ts
@@ -1,5 +1,9 @@
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    // ビルド時の ESLint エラーを無視する
+    ignoreDuringBuilds: true,
+  },
   // pagesDir: 'pages', // pages ディレクトリを使用しない場合はコメントアウトまたは削除
   env: {
     API_URL: process.env.API_URL,
